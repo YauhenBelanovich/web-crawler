941d965f4231fc9563a5b0ca496189f5ff441865
Yauhen Belanovich <yauhen2012@gmail.com>
2021-01-06 15:58:27 +0300

    empty project

---
+++ spring-boot-module/src/main/java/com/gmail/yauhen2012/springbootmodule/runner/MyApplicationRunner.java
@@ -0,0 +1,15 @@
+package com.gmail.yauhen2012.springbootmodule.runner;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyApplicationRunner implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+
+}
