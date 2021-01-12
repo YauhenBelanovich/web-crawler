74853663dda4760c94191afcb9df4e756a2606ac
Yauhen Belanovich <yauhen2012@gmail.com>
2021-01-12 18:15:10 +0300

    proxy

--- nodejs-server/server.js
+++ nodejs-server/server.js
@@ -10,5 +10,8 @@ app.listen(port, () => {
   console.log('App listening at http://localhost:' + port)
 })
 
-app.use('/api', proxy('http://localhost:8081'));
+//app.use('/api', proxy('http://localhost:8081'));
+
+app.use('/api', proxy('http://10.0.2.15:8081'));
+
 
